6eb1ce8446a7f88a30b66bd67032b52c614dfd27
acer <acer@BASIL>
2025-07-17 00:10:00 +0530

    Change for samplesuper package

--- JavaPrograms/src/samplesuper/Child.java
+++ JavaPrograms/src/samplesuper/Child.java
@@ -21,7 +21,7 @@ public class Child extends Parent{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Child obj = new Child();
-		//obj.childSum();
+		obj.childSum();
 		
 	}
 
